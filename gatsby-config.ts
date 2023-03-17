f0d3586d014bef4a57dfb79ee26851394260e4e2
NaxeCode <siraj.n.lee@gmail.com>
2023-03-17 04:28:43 -0700

    Added requirements for gatsby-optimized image

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,6 +12,13 @@ const config: GatsbyConfig = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
   ],
 }
 
