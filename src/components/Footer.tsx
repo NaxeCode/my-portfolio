3c0bea0d76fba7d02056dbb633034d521d5b4e4d
NaxeCode <siraj.n.lee@gmail.com>
2023-03-19 10:39:42 -0700

    Adjusted footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import '../styles/Styles.css';
 
 function Footer() {
-    return (
-        <>
-            <h2>
-                Contact me!
-            </h2>
-        </>
-    );
+  return (
+    <footer>
+      <p>Thanks for checking out my website!</p>
+    </footer>
+  );
 }
 
 export default Footer;
