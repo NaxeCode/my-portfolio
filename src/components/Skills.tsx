ba99398a2cbb357f94d2f0205f6256a5fbf4f94d
NaxeCode <siraj.n.lee@gmail.com>
2023-03-19 10:39:10 -0700

    Adjusted the skills and simplified them

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import '../styles/Styles.css';
 
-const paragraphStyles = {
-    color: '#f1faee',
-    marginBottom: 48,
-};
-
-function Skills() {
-    return (
-        <>
-            <h2>Skills!</h2>
-            <ul>
-                <li style={paragraphStyles}>Programming Languages</li>
-                <ul>
-                    <li>C#</li>
-                    <li>Golang</li>
-                    <li>Typescript/JavaScript</li>
-                    <li>GameMaker Language</li>
-                    <li>Lua</li>
-                    <li>Python</li>
-                    <li>PHP</li>
-                </ul>
-            </ul>
-        </>
-    );
+function Skills(props) {
+  return (
+    <section>
+      <h2>Skills!</h2>
+      <ul>
+        {props.skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default Skills;
