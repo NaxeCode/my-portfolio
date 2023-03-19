25e66b7e33d76306fb7c2ab7a720d72bc540ac77
NaxeCode <siraj.n.lee@gmail.com>
2023-03-19 12:02:27 -0700

    Added in temp image icon for work component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Who from '../components/Who';
 import Skills from '../components/Skills';
 import Work from '../components/Work';
 import Footer from '../components/Footer';
+import port from "../images/icon.png";
 
 const mainStyle = {
   backgroundColor: "#264653",
@@ -39,9 +40,16 @@ const IndexPage: React.FC<PageProps> = () => {
       <Who intro="Welcome to my website! I'm a software engineer and game programmer based in Portland, Oregon USA. With my experience working on indie and live service games. This site showcases results of my passion and dedication, featuring a collection of games and software projects. From crafting engaging gameplay mechanics to designing elegant code architecture, I pour my heart and soul into every project. Take a look around my portfolio and feel free to get in touch with me to learn more about my work or to discuss potential collaborations." />
       <Skills skills={['JavaScript', 'React', 'HTML', 'CSS']} />
       <Work projects={[
-        { name: 'Project 1', image: 'project1.jpg' },
-        { name: 'Project 2', image: 'project2.jpg' },
-        { name: 'Project 3', image: 'project3.jpg' },
+        { name: 'Project 1', image: port },
+        { name: 'Project 2', image: port },
+        { name: 'Project 3', image: port },
+        { name: 'Project 4', image: port },
+        { name: 'Project 5', image: port },
+        { name: 'Project 6', image: port },
+        { name: 'Project 7', image: port },
+        { name: 'Project 8', image: port },
+        { name: 'Project 9', image: port },
+        { name: 'Project 10', image: port },
       ]} />
       <Footer />
     </div>
