c867a7e1a62cf30957d8dcbd7791f29f4a324ef5
NaxeCode <siraj.n.lee@gmail.com>
2023-03-21 14:30:43 -0700

    Added custom theme colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				headerBG: '#242423',
+				mainBG: '#333533',
+				author: '#F5CB5C',
+				header: '#E8EDDF',
+				paragraph: '#CFDBD5',
+			},
+		},
 	},
 	variants: {
 		extend: {},
