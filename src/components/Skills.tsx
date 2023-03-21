b4b85b5bf9ed39f5394a4b548d6888f2a1776ce2
NaxeCode <siraj.n.lee@gmail.com>
2023-03-21 15:41:54 -0700

    Slight adjustments, removed !

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,21 +9,21 @@ interface SkillsProps {
 function Skills(props: SkillsProps) {
   return (
     <section>
-      <h2>Programming!</h2>
+      <h2>Programming</h2>
       <ul className="list-wrapper list-disc">
         {props.programming.map((programming, index) => (
           <li key={index}>{programming}</li>
         ))}
       </ul>
 
-      <h2>Frameworks!</h2>
+      <h2>Frameworks</h2>
       <ul className="list-wrapper">
         {props.frameworks.map((frameworks, index) => (
           <li key={index}>{frameworks}</li>
         ))}
       </ul>
 
-      <h2>Engines!</h2>
+      <h2>Engines</h2>
       <ul className="list-wrapper">
         {props.engines.map((engines, index) => (
           <li key={index}>{engines}</li>
