d17059af39b8d07f9b4ba8ae4c75252ce51221e2
NaxeCode <siraj.n.lee@gmail.com>
2023-03-24 13:36:44 -0700

    Added a new element, adjusted rounding to be more pleasing

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,33 +10,47 @@ interface SkillsProps {
 function Skills(props: SkillsProps) {
   return (
     <section>
+
       <h2>Programming</h2>
-      <ul className="list-wrapper list-disc">
-        {props.programming.map((programming, index) => (
-          <li key={index}>{programming}</li>
-        ))}
-      </ul>
+
+      <div className='roundedBG'>
+        <ul className="list-wrapper list-disc">
+          {props.programming.map((programming, index) => (
+            <li key={index}>{programming}</li>
+          ))}
+        </ul>
+      </div>
+
 
       <h2>Frameworks</h2>
-      <ul className="list-wrapper">
-        {props.frameworks.map((frameworks, index) => (
-          <li key={index}>{frameworks}</li>
-        ))}
-      </ul>
+      <div className='roundedBG'>
+        <ul className="list-wrapper">
+          {props.frameworks.map((frameworks, index) => (
+            <li key={index}>{frameworks}</li>
+          ))}
+        </ul>
+      </div>
+
 
       <h2>Engines</h2>
-      <ul className="list-wrapper">
-        {props.engines.map((engines, index) => (
-          <li key={index}>{engines}</li>
-        ))}
-      </ul>
+      <div className='roundedBG'>
+        <ul className="list-wrapper">
+          {props.engines.map((engines, index) => (
+            <li key={index}>{engines}</li>
+          ))}
+        </ul>
+      </div>
+
 
       <h2>Areas of Development</h2>
-      <ul className="list-wrapper">
-        {props.areas.map((areas, index) => (
-          <li key={index}>{areas}</li>
-        ))}
-      </ul>
+      <div className='roundedBG'>
+        <ul className="list-wrapper">
+          {props.areas.map((areas, index) => (
+            <li key={index}>{areas}</li>
+          ))}
+        </ul>
+      </div>
+
     </section>
   );
 }
