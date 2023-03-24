4eac231ba478b93c6eeab9ef5a491800150c605d
NaxeCode <siraj.n.lee@gmail.com>
2023-03-24 13:18:38 -0700

    Imported images, added Areas of Development section, added more frameworks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,29 +6,34 @@ import Skills from '../components/Skills';
 import Work from '../components/Work';
 import Footer from '../components/Footer';
 import port from "../images/icon.png";
+import soulCAT from "../images/soulCat.png";
+import magnum from "../images/magnum.png";
+import hop from "../images/hop.png";
+import dodge from "../images/dodge.png";
+import doll from "../images/doll.png";
+import vapor from "../images/vapor.png";
+import eggs from "../images/eggs.png";
 import '../styles/tailwind.css';
 
 const introText = "Welcome to my website! I'm a software engineer and game programmer based in Portland, Oregon. This site showcases my collection of games and software projects. From crafting engaging gameplay mechanics to designing elegant code architecture, I pour my heart and soul into every project. Take a look around and feel free to get in touch with me to learn more or discuss potential collaborations.";
-const languages = ['Haxe', 'C++', 'C#', 'HTML', 'CSS'];
-const frameworks = ['HaxeFlixel', '.Net Core', 'React', "Gatsby"];
-const engines = ['XNA / FNA', 'Unity', 'Unreal', 'React', 'Unity'];
+const languages = ['Haxe', 'C++', 'C#', 'HTML', 'CSS', "JavaScript / Typescript", "Lua", "Golang", "Python"];
+const frameworks = ['HaxeFlixel', '.Net Core', "Xamarin", 'React', "Gatsby", "Phaser.io", "PixiJS", "Game Maker"];
+const engines = ['XNA / FNA', 'Unity', 'Unreal 4', 'React', 'Unity', "Godot"];
+const areas = ["Git", "Online Networking", "AWS", "Azure", "Steamworks SDK", "Kubernetes & Docker", "Cloud Architecture", "Web UI / UX", "Game Design", "Technical Writing", "Mobile Porting (IOS & Android)", "Game Engine Design"];
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <div>
       <Header name="NaxeCode"/>
       <Who intro={introText} />
-      <Skills programming={languages} frameworks={frameworks} engines={engines} />
+      <Skills programming={languages} frameworks={frameworks} engines={engines} areas={areas} />
       <Work projects={[
-        { name: 'Project 1', image: port },
-        { name: 'Project 2', image: port },
-        { name: 'Project 3', image: port },
-        { name: 'Project 4', image: port },
-        { name: 'Project 5', image: port },
-        { name: 'Project 6', image: port },
-        { name: 'Project 7', image: port },
-        { name: 'Project 8', image: port },
-        { name: 'Project 9', image: port },
-        { name: 'Project 10', image: port },
+        { name: 'i ate two eggs', image: eggs },
+        { name: 'Vaporwave Breakout', image: vapor},
+        { name: 'Dust Drop Doll', image: doll },
+        { name: 'Dodge Knight', image: dodge },
+        { name: 'Hop: Friend Ressurection', image: hop },
+        { name: 'Magnum', image: magnum },
+        { name: 'Soul Cat', image: soulCAT },
       ]} />
       <Footer />
     </div>
